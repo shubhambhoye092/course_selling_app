c4e5af5bfb8a76352c20650456c24d9c9595c966
Shubham Bhoye <shubhambhoye92@gmail.com>
2025-07-08 23:08:30 +0530

    added routers

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 const express = require('express');
+const { userRouter } = require('./routes/user');
+const { createCourseRoutes } = require('./routes/course');
 const app = express();
 
-app.post("/user/signup", function(req, res) {
-    res.json({
-        message: "signup endpoint"})
-});
+app.use("/user", userRouter);
+app.use("/course", courseRouter);
 
-app.post("/user/signin", function(req, res) {
-    res.json({
-        message: "signin endpoint"})
-});
-
-app.get("/user/purchases", function(req, res) {
-    res.json({
-        message: "signin endpoint"})
-});
-
-app.post("/course/purchases", function(req, res) {
-    res.json({
-        message: "signin endpoint"})
-});
-
-app.get("/courses", function(req, res) {
-    res.json({
-        message: "signup endpoint"})
-});
-
+createCourseRoutes(app);
+createUserRoutes(app);
 
 app.listen(3000);
---
+++ routes/course.js
@@ -0,0 +1,10 @@
+
+app.post("/course/purchases", function(req, res) {
+    res.json({
+        message: "signin endpoint"})
+});
+
+app.get("/course", function(req, res) {
+    res.json({
+        message: "signup endpoint"})
+});
